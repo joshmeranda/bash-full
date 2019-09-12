56e28dc7f11499f245754dfb5d87b5f7fe8f521c
josh <joshmeranda@gmail.com>
2019-09-12 13:20:22 -0400

    Watch for files with inotify

--- tstmate.sh
+++ tstmate.sh
@@ -8,8 +8,8 @@ SCRIPT_NAME="$(basename "$0")"
 
 usage() {
 echo "Usage: $SCRIPT_NAME --project PATH --script SCRIPT
-I am the usage text.
-I am another line.
+  -p --project    The path to the tagre project.
+  -s --script     The testing script to run on file change.
 "
 }
 
@@ -17,16 +17,16 @@ echo_err() {
     echo -e "$SCRIPT_NAME: $1" 2>&1
 }
 
-if [ "$#" -eq 0 ]; then echo_err "missing operands.\nTry '$SCRIPT_NAME --help' for help"; fi
+if [ "$#" -eq 0 ]; then echo_err "missing operands.\nTry '$SCRIPT_NAME --help' for help"; exit 1; fi
 
 # Parse arguments.
-while true; do
+while [ "$#" -gt 0 ]; do
     case "$1" in
         --help)
             usage
             exit 0
             ;;
-        -s | --script)
+        -t | --test-script)
             script="$2"
             ;;
         -p | --project)
@@ -38,14 +38,29 @@ while true; do
 done
 
 # Ensure both a project and testing scritpt were specified.
-if [ -z "$project" ]; then echo_err "no project path specified"; fi
-if [ -z "$script" ]; then echo_err "no test script specified"; fi
+if [ -z "$project" ]; then echo_err "no project path specified"; exit 1; fi
+if [ -z "$script" ]; then echo_err "no test script specified"; exit 1; fi
 
+# TODO Parse .gitignore for what to ignore
 while true; do
-    watch --chgexit --no-title \\
-        cat "$project"/*
+    # Find all project files to watch for changes
+    find "$project" -type f -not \( \
+            -path '*/\.git/*'       \
+            -o -path '*/\.svn/*'    \
+            -o -path "*/*\.iml"     \
+            -o -path '*/\.idea/*'   \
+            -o -path '*/target/*'   \
+        \)                          \
+        -exec inotifywait --event modify {} +
 
-        if ! eval "$script"; then break; fi
+        # Run testing script after MODIFY event is detected
+        if [ -f "$script" ]; then
+            evel < "$script"
+        else
+            eval "$script"
+        fi
+
+        break
 done
 
-return "$?"
+exit "$?"
